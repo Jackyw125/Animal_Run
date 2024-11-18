6653de5edb3b294d177e540fa3206d1efd605958
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-11-18 16:09:03 -0700

    Add files via upload

---
+++ Game/Stage 5/MODEL.C
@@ -0,0 +1,263 @@
+#include "model.h"
+#include <stdlib.h>  
+
+/***********************************************************************
+ * Name: initialize_model
+ *
+ * Purpose:
+ *   Initializes all components of the game model, setting up the initial 
+ *   state for the game.
+ *
+ * Details:
+ *   This function initializes various elements of the game model, 
+ *   including the player character (e.g., chicken), monsters, coins, 
+ *   score, and ground.
+ *
+ * Parameters:
+ *   - model: Pointer to the game model structure to be initialized.
+ ***********************************************************************/
+void initialize_model(Model *model)
+{
+    initialize_animal(&(model->chicken));
+    initialize_monster((model->monster), (model->coins));
+    initialize_coins((model->coins), (model->monster));
+    initialize_score(&(model->score));
+    initialize_ground(&(model->ground), GROUND_Y); 
+}
+
+/***********************************************************************
+* Name: initialize_animal
+*
+* Purpose: Initializes the game model with default values.
+*
+* Details: Initializes the animal character.
+*
+* Parameters:
+*     - model: Pointer to the game model to be initialized.
+***********************************************************************/
+void initialize_animal(Animal *chicken)
+{
+    chicken->x = 10;
+    chicken->y = (GROUND_Y) - 62;
+    chicken->prev_x = -1;
+    chicken->prev_y = -1;
+    chicken->velocity = ANIMAL_HORIZONTAL_MOVEMENT;
+    chicken->isFalling = false;
+    chicken->max_y = chicken->y;
+    chicken->dead = false;
+}
+
+/***********************************************************************
+* Name: initialize_monster
+*
+* Purpose: Initializes the monster model with default values.
+*
+* Details: Initializes the monsters.
+*
+* Parameters:
+*     - model: Pointer to the game model to be initialized.
+***********************************************************************/
+void initialize_monster(Monster *monster, Coin *coins) {
+    int i, j;
+    bool isColliding;
+
+    for (i = 0; i < MAX_MONSTER; i++) {
+        do {
+            /*Randomly place the monster within the valid horizontal and vertical bounds*/
+            monster[i].x = rand();
+            monster[i].y = rand();
+            monster[i].off_screen = false;
+
+             isColliding = false;
+
+            /*Check collision with coins*/
+        for (j = 0; j < MAX_COINS; j++) {
+            if (check_object_collision(monster[i].x, monster[i].y, MONSTER_WIDTH, MONSTER_HEIGHT,
+                                       coins[j].x, coins[j].y, COIN_WIDTH, COIN_HEIGHT)) {
+                isColliding = true;
+                break;
+            }
+        }
+
+        /*Check collision with other monsters*/
+        for (j = 0; j < i; j++) {
+            if (check_object_collision(monster[i].x, monster[i].y, MONSTER_WIDTH, MONSTER_HEIGHT,
+                                       monster[j].x, monster[j].y, MONSTER_WIDTH, MONSTER_HEIGHT)) {
+                isColliding = true;
+                break;
+            }
+        }
+
+        /*Ensure the monster stays within screen boundaries*/
+        if (monster[i].y + MONSTER_HEIGHT > GROUND_Y || monster[i].y < 0) {
+            isColliding = true;
+        }
+
+        } while (isColliding);
+    }
+}
+
+/***********************************************************************
+* Name: initialize_score
+*
+* Purpose: Initializes the coin model with default values.
+*
+* Details: Initializes the coins.
+*
+* Parameters:
+*     - model: Pointer to the game model to be initialized.
+***********************************************************************/
+
+void initialize_coins(Coin *coins, Monster *monster) {
+    int j, k;
+    bool isColliding;
+
+    for (j = 0; j < MAX_COINS; j++) {
+        do {
+            /*Randomly place the coin within valid horizontal and vertical bounds*/
+            coins[j].x = rand() % SCREEN_WIDTH;
+            coins[j].y = rand() % (GROUND_Y - COIN_HEIGHT);
+            coins[j].active = true;
+
+            isColliding = false;
+
+            /*Check collision with monsters*/
+            for (k = 0; k < MAX_MONSTER; k++) {
+                if (check_object_collision(coins[j].x, coins[j].y, COIN_WIDTH, COIN_HEIGHT,
+                                           monster[k].x, monster[k].y, MONSTER_WIDTH, MONSTER_HEIGHT)) {
+                    isColliding = true;
+                    break;
+                }
+            }
+            /*Ensure the coin stays within screen boundaries*/
+            if (coins[j].y + COIN_HEIGHT > GROUND_Y || coins[j].y < 0) {
+                isColliding = true;
+            }
+
+        } while (isColliding);
+    }
+}
+
+/***********************************************************************
+* Name: initialize_score
+*
+* Purpose: Initializes the score model with default values.
+*
+* Details: Initializes the score.
+*
+* Parameters:
+*     - score: Pointer to the score to be initialized.
+***********************************************************************/
+void initialize_score(Score *score)
+{
+    score->digits = 0;
+    score->total = 0;
+    score->prev_total = 0;
+}
+
+/***********************************************************************
+* Name: initialize_ground
+*
+* Purpose: Initializes the ground model with default values.
+*
+* Details: Initializes the ground.
+*
+* Parameters:
+*     - model: Pointer to the game model to be initialized.
+***********************************************************************/
+void initialize_ground(Ground *ground, int y) 
+{
+	ground->y = y;
+}
+
+/***********************************************************************
+* Name: move_animal
+*
+* Purpose: Moves the animal character.
+*
+* Details: Moves the animal character horizontally and vertically
+*          based on the given displacement. Updates the previous
+*          position.
+*
+* Parameters:
+*     - doodle: Pointer to the animal object to be moved.
+*     - displacement_x: Horizontal displacement.
+*     - displacement_y: Vertical displacement.
+***********************************************************************/
+void move_animal(Animal *chicken, int displacement_x, int displacement_y)
+{
+    int screenBorderCheck;
+  
+    chicken->prev_x = chicken->x;
+    chicken->prev_y = chicken->y;
+
+    chicken->x += displacement_x;
+    chicken->y += displacement_y;
+}
+
+/***********************************************************************
+* Name: animal_horizontal_movement
+*
+* Purpose: Handles horizontal movement of the animal.
+*
+* Details: Handles animal's horizontal movement, including falling,
+*          jumping, and collision detection with coins.
+*
+* Parameters:
+*     - model: Pointer to the game model.
+***********************************************************************/
+void animal_horizontal_movement(Model *model)
+{
+    model->chicken.prev_x = model->chicken.x;
+    move_animal(&(model->chicken), ANIMAL_HORIZONTAL_MOVEMENT, 0);
+    if (model->chicken.x > 640) {
+        model->chicken.x = 0;
+    }
+}
+
+/***********************************************************************
+* Name: has_animal_moved
+*
+* Purpose: Checks if the animal has moved.
+*
+* Details: Compares the current animal position
+*          with the previous state to determine if there's movement.
+*
+* Parameters:
+*     - animal: Pointer to the animal object.
+* Returns:
+*     - bool: True if the animal has moved, false otherwise.
+***********************************************************************/
+bool has_animal_moved(Animal *chicken)
+{
+    if(chicken->prev_x != chicken->x || chicken->prev_y != chicken->y)
+        return true;
+
+    return false;
+}
+
+
+/***********************************************************************
+ * Name: check_object_collision
+ *
+ * Purpose:
+ *   Checks for a collision between two rectangular objects.
+ *
+ * Details:
+ *   Determines whether two rectangles overlap based on their 
+ *   coordinates and dimensions. 
+ *
+ * Parameters:
+ *   - x, y: Coordinates of the top-left corner of the first rectangle.
+ *   - width, height: Dimensions of the first rectangle.
+ *   - x2, y2: Coordinates of the top-left corner of the second rectangle.
+ *   - width2, height2: Dimensions of the second rectangle.
+ *
+ * Return Value:
+ *   - `true` if the rectangles overlap (collision detected).
+ *   - `false` otherwise (no collision).
+ ***********************************************************************/
+bool check_object_collision(int x, int y, int width, int height, int x2, int y2, int width2, int height2) {
+    return (x < x2 + width2) && (x + width > x2) &&
+           (y < y2 + height2) && (y + height > y2);
+}
---
+++ Game/Stage 5/RENDER.C
@@ -0,0 +1,140 @@
+#include "RENDER.H"
+
+/***********************************************************************
+ * Name: render
+ *
+ * Purpose:
+ *   Renders the game model's current state onto the screen buffer.
+ *
+ * Details:
+ *   The function handles rendering all visible components of the game, 
+ *   including the player's character, monsters, coins, score, and ground. 
+ *   It also ensures proper handling of object movement and collision:
+ *     - Clears previous positions of the player and coins after movement.
+ *     - Renders updated positions for the player, monsters, and coins.
+ *     - Deactivates coins and clears their bitmap from the screen if 
+ *       collected by the player.
+ *     - Continuously updates and renders the game score and ground.
+ *
+ * Parameters:
+ *   - model: Pointer to the game model structure containing game state 
+ *            information (player, coins, monsters, score, ground, etc.).
+ *   - base: Pointer to the base address of the screen buffer.
+ ***********************************************************************/
+void render(Model *model, UINT32 *base)
+{
+    int i;
+    if (has_animal_moved(&(model->chicken))) {   
+        clear_bitmap_32(base, model->chicken.prev_x, model->chicken.prev_y, clear_bitmap, CHICKEN_HEIGHT);
+        render_animal(&(model->chicken), base);
+    }
+
+    render_monster(model->monster, base);
+    render_coin(model->coins, (UINT16 *)base);
+
+    render_score(&(model->score), base); 
+    render_ground(&(model->ground), (UINT8 *)base);
+}
+
+/***********************************************************************
+* Name: render_score
+*
+* Purpose: Renders the game score onto the screen buffer.
+*
+* Details: Renders the score onto the screen buffer using bitmap
+*          plotting. Clears previous digits before rendering new ones.
+*
+* Parameters:
+*     - score: Pointer to the game score structure.
+*     - base: Pointer to the base address of the screen buffer.
+***********************************************************************/
+void render_score(Score *score, UINT32 *base)
+{
+    int temp = score->total;
+    int digitPosition = 0;
+    int digits[10];  
+    int i;
+
+    if (temp == 0) {
+        clear_bitmap_8((UINT16*)base, digitPosition << 4, 8, char_delete, FONT_HEIGHT);
+        plot_bitmap_8((UINT16*)base, digitPosition << 4, 8, GLYPH_START('0'), FONT_HEIGHT);
+        return;
+    }
+
+    while (temp > 0) {
+        digits[digitPosition++] = temp % 10; 
+        temp /= 10;  
+    }
+
+    for (i = digitPosition - 1; i >= 0; i--) {
+        clear_bitmap_8((UINT16*)base, (digitPosition - 1 - i) << 4, 8, char_delete, FONT_HEIGHT);
+        plot_bitmap_8((UINT16*)base, (digitPosition - 1 - i) << 4, 8, GLYPH_START(digits[i] + '0'), FONT_HEIGHT);
+    }
+}
+
+
+
+/***********************************************************************
+* Name: render_animal
+*
+* Purpose: Renders the animal onto the screen buffer.
+*
+* Details: Renders the animal onto the screen buffer based on its
+*          position.
+* Parameters:
+*     - animal: Pointer to the animal object.
+*     - base: Pointer to the base address of the screen buffer.
+***********************************************************************/
+
+void render_animal(Animal *chicken, UINT32 *base)
+{
+        plot_bitmap_32(base, chicken->x, chicken->y, chicken_bitmap, CHICKEN_HEIGHT);
+}
+
+/***********************************************************************
+* Name: render_monster
+*
+* Purpose: Renders the monster onto the screen buffer.
+*
+* Details: Renders the monster onto the screen buffer based on its
+*          position using bitmap plotting.
+*
+* Parameters:
+*     - monster: Pointer to the monster object.
+*     - base: Pointer to the base address of the screen buffer.
+***********************************************************************/
+void render_monster(Monster *monster, UINT32 *base)
+{
+    int i;
+    for (i = 0; i < MAX_MONSTER; i++) {
+        if (!monster[i].off_screen) {  
+            plot_bitmap_32(base, monster[i].x, monster[i].y, monster_bitmap, MONSTER_HEIGHT);
+        }
+    }
+}
+
+/***********************************************************************
+* Name: render_coin
+*
+* Purpose: Renders the score onto the screen buffer.
+*
+* Details: Renders the coin onto the screen buffer based on its
+*          position using bitmap plotting.
+*
+* Parameters:
+*     - coin: Pointer to the coin object.
+*     - base: Pointer to the base address of the screen buffer.
+***********************************************************************/
+void render_coin(Coin *coin, UINT16 *base)
+{
+    int i;
+    for (i = 0; i < MAX_COINS; i++) {
+        if (coin[i].active) {  
+            plot_bitmap_16(base, coin[i].x, coin[i].y, coin_bitmap, COIN_HEIGHT);
+        }
+    }
+}
+
+void render_ground(Ground *ground, UINT8 *base) {
+    plot_horizontal_line(base, ground->y);  
+}
