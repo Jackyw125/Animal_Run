a97c0e75b3a77af4536f27f3bba101090a015311
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-11-18 16:10:32 -0700

    Add files via upload

---
+++ Game/Stage 6/EVENTS.C
@@ -0,0 +1,208 @@
+#include "events.h"
+
+/***********************************************************************
+* Name: check_animal_death
+*
+* Purpose: Checks if the animal has died.
+*
+* Details: Checks if the animal has collided with the monster.
+*
+* Parameters:
+*     - model: Pointer to the game model.
+*     - endGame: Pointer to a boolean indicating game end.
+***********************************************************************/
+void check_animal_death(Model *model, bool *endGame)
+{
+    int i; 
+    for (i = 0; i < MAX_MONSTER; i++) {
+        if (check_collision_monster(&(model->chicken), &(model->monster[i]))) {
+            model->chicken.dead = true;
+            *endGame = true;
+        }
+    }
+    return;
+}
+
+/***********************************************************************
+* Name: animal_input
+*
+* Purpose: Handles user input for animal movement.
+*
+* Details: If the spacebar is pressed, moves the animal vertically.
+*          If the animal is dead, no movement is allowed.
+*
+* Parameters:
+*     - chicken: Pointer to the animal object.
+*     - key: chicken representing the pressed key.
+***********************************************************************/
+void animal_input(Animal *chicken, char key)
+{
+    if(chicken->dead)
+        return;
+
+    if(key == ' '){
+        animal_jump(chicken);
+    }
+
+}
+
+/***********************************************************************
+* Name: check_collision_coin
+*
+* Purpose: Checks collision between animal and coins.
+*
+* Details: Checks if the animal collides with any of the coins.
+*
+* Parameters:
+*     - animal: Pointer to the animal object.
+*     - coin: Pointer to the array of coin objects.
+* Returns:
+*     - bool: True if collision occurs, false otherwise.
+***********************************************************************/
+bool check_collision_coin(Animal *chicken, Coin *coins) {
+    int i;
+    for (i = 0; i < MAX_COINS; i++) {
+        if (coins[i].active) {
+            bool horizontal_overlap = (chicken->x + CHICKEN_WIDTH > coins[i].x) &&
+                                      (chicken->x < coins[i].x + COIN_WIDTH);
+
+            bool vertical_overlap = (chicken->y + CHICKEN_HEIGHT > coins[i].y) &&
+                                    (chicken->y < coins[i].y + COIN_HEIGHT);
+
+            if (horizontal_overlap && vertical_overlap) {
+                return true; 
+            }
+        }
+    }
+    return false; 
+}
+
+/***********************************************************************
+* Name: check_collision_monster
+*
+* Purpose: Checks collision between animal and monster.
+*
+* Details: Checks if the animal collides with the monster.
+*
+* Parameters:
+*     - animal: Pointer to the animal object.
+*     - monster: Pointer to the monster object.
+* Returns:
+*     - bool: True if collision occurs, false otherwise.
+***********************************************************************/
+
+bool check_collision_monster(Animal *chicken, Monster *monster)
+{
+    int i; 
+    for (i = 0; i < MAX_MONSTER; i++) {
+        bool horizontal_overlap = (chicken->x + CHICKEN_WIDTH > monster[i].x) &&
+                                  (chicken->x < monster[i].x + MONSTER_WIDTH);
+
+        bool vertical_overlap = (chicken->y + CHICKEN_HEIGHT > monster[i].y) &&
+                                (chicken->y < monster[i].y + MONSTER_HEIGHT);
+
+        if (horizontal_overlap && vertical_overlap) {
+            return true; 
+        }
+    }
+    return false;
+}
+
+/***********************************************************************
+* Name: check_ground_collision
+*
+* Purpose: Checks collision between animal and ground.
+*
+* Details: Checks if the animal collides with the ground.
+*
+* Parameters:
+*     - animal: Pointer to the animal object.
+*     - ground: Pointer to the ground object.
+* Returns:
+*     - bool: True if collision occurs, false otherwise.
+***********************************************************************/
+bool check_ground_collision(Animal *chicken, Ground* ground) {
+    if (chicken->y ==  ground->y) 
+    {
+        return true;
+    }
+    return false;
+}
+
+void animal_vertical_movement(Model *model)
+{
+    Animal *chicken = &(model->chicken);
+
+    switch (chicken->state) {
+        case ANIMAL_STATE_MOVING_UP:
+            chicken->max_y -= MAX_VERT_VELOCITY;
+
+            chicken->y -= chicken->max_y;
+            if (chicken->max_y <= 0) {
+                chicken->state = ANIMAL_STATE_MOVING_DOWN;
+            }
+            break;
+
+        case ANIMAL_STATE_MOVING_DOWN:
+      
+            chicken->y += chicken->velocity;
+
+            chicken->max_y += MAX_VERT_VELOCITY;
+
+            if (chicken->y + CHICKEN_HEIGHT >= GROUND_Y) {
+                chicken->y = GROUND_Y - CHICKEN_HEIGHT;
+                chicken->max_y = 0;
+                chicken->state = ANIMAL_STATE_ON_GROUND;
+            }
+            break;
+
+        case ANIMAL_STATE_ON_GROUND:
+            chicken->max_y= 0;
+            break;
+
+        default:
+            break;
+    }
+    if (chicken->y < 0) {
+        chicken->y = 0;
+        chicken->max_y = 0;
+        chicken->state = ANIMAL_STATE_MOVING_DOWN;
+    }
+}
+
+void animal_jump(Animal *chicken)
+{
+    if (chicken->state == ANIMAL_STATE_ON_GROUND) {
+        chicken->max_y = MAX_VERT_VELOCITY; 
+        chicken->state = ANIMAL_STATE_MOVING_UP;
+    }
+}
+
+/***********************************************************************
+ * Function Name: update_score
+ *
+ * Purpose:
+ *   Updates the score in the game model when a collision is detected 
+ *   between the player's character (e.g., chicken) and a coin.
+ *
+ * Parameters:
+ *   - model: Pointer to the game model structure containing the 
+ *            player character, coins, and score data.
+ *
+ * Details:
+ *   This function is responsible for handling score increments when
+ *   the player collects a coin. It checks for a collision using the 
+ *   `check_collision_coin` function. If a collision is detected:
+ ***********************************************************************/
+void update_score(Model *model)
+{
+    int i;
+    for (i = 0; i < MAX_COINS; i++) {
+        if (model->coins[i].active && 
+            check_object_collision(model->chicken.x, model->chicken.y, CHICKEN_WIDTH, CHICKEN_HEIGHT,
+                                   model->coins[i].x, model->coins[i].y, COIN_WIDTH, COIN_HEIGHT)) {
+            model->coins[i].active = false;  
+            model->score.total++;  
+        }
+    }
+}
---
+++ Game/Stage 6/MAIN.C
@@ -0,0 +1,243 @@
+#include "MODEL.H"
+#include "RENDER.H"
+#include "EVENTS.H"
+#include <osbind.h>
+#include <stdio.h>
+
+
+UINT8 double_buffer[448][80] = {0};
+
+
+UINT32 get_time();
+void input(Model *model, char *pressedKey);
+void syncModel(Model *modelSrc, Model *modelDst);
+void process_synchronous_events(Model *model, bool *endGame, int seed);
+
+
+int main() {
+UINT8 *page1 = Physbase();
+    UINT8 *page2 = &double_buffer[0][0];
+    UINT8 confirmedInput;
+    int seed;
+
+
+    bool useDoubleBuffer = true;
+    bool endGame = false;
+
+
+    UINT32 timeThen, timeNow, timeElapsed;
+
+
+    Model modelOne, modelTwo, modelThree;
+    Model *modelPtr = &modelOne;
+    Model *modelSnapshotOne = &modelTwo;
+    Model *modelSnapshotTwo = &modelThree;
+    char pressedKey = 0;
+
+
+    seed = Random();
+
+
+    initialize_model(modelPtr);
+    initialize_model(modelSnapshotOne);
+    initialize_model(modelSnapshotTwo);
+
+
+    page2 = (UINT8*)((size_t)page2 | 0xff ) + 1; /* finding page aligned address for second page*/
+
+
+    /* prepering models for main game loop*/
+    syncModel(modelPtr, modelSnapshotOne);
+    syncModel(modelPtr, modelSnapshotTwo);
+
+
+    /* preparing models for main game loop */
+    clear_screen(page1);
+    clear_screen(page2);
+    render(modelPtr, (UINT32*)page1);
+    render(modelPtr, (UINT32*)page2);
+    timeThen = get_time();
+
+
+    while (pressedKey != 'q' && !endGame) { /* Main game loop */
+
+
+        input(modelPtr, &pressedKey);
+
+
+        timeNow = get_time();
+        timeElapsed = timeNow - timeThen;
+
+
+        if(timeElapsed > 0)
+        {
+            Vsync();
+            if(useDoubleBuffer)
+                {
+                    process_synchronous_events(modelPtr, &endGame, seed);
+                   
+                    syncModel(modelPtr, modelSnapshotOne);
+                    double_buffer_render(modelSnapshotTwo, (UINT32*)page1);
+                    update_score(modelPtr);
+
+                    Setscreen(-1, page1, -1);
+                   
+                    useDoubleBuffer = false;
+                }
+                else
+                {
+                    process_synchronous_events(modelPtr, &endGame, seed);
+
+                    syncModel(modelPtr, modelSnapshotTwo);
+                    double_buffer_render(modelSnapshotOne,(UINT32*)page2);
+                    update_score(modelPtr);
+                   
+                    Setscreen(-1, page2, -1);
+                   
+                    useDoubleBuffer = true;
+                }
+                seed = seed*3;
+                Vsync();
+            }
+            timeThen = get_time();
+        }
+    clear_screen(page1);
+    Vsync();
+    return 0;
+}
+
+
+/***********************************************************************
+* Name: input
+*
+* Purpose: Handles user input for the game.
+*
+* Details: Checks if keyboard input is available and reads the key.
+*          If the pressed key is 'a', 'd', or 'q', it updates the
+*          pressedKey variable accordingly.
+*
+* Parameters:
+*     - model: Pointer to the game model.
+*     - pressedKey: Pointer to the variable storing the pressed key.
+***********************************************************************/
+void input(Model *model, char *pressedKey)
+{
+    if (Cconis()) /* Check if keyboard input is available */
+    {
+        char key = (char)Cnecin(); /* Read keyboard input */
+        if (key == ' ' || key == 'q') {
+            *pressedKey = key;
+        }
+    }
+    else {
+        *pressedKey = 0; /* Reset pressedKey if no key is pressed */
+    }
+
+    animal_input(&(model->chicken), *pressedKey);
+}
+
+
+/***********************************************************************
+* Name: syncModel
+*
+* Purpose: Synchronizes the content of two game models.
+*
+* Details: Copies data from the source model to the destination model,
+*          ensuring consistency between the two models.
+*
+* Parameters:
+*     - modelSrc: Pointer to the source game model.
+*     - modelDst: Pointer to the destination game model.
+***********************************************************************/
+void syncModel(Model *modelSrc, Model *modelDst)
+{
+    UINT8 i;
+    UINT8 j;
+    Coin *srcCoin, *dstCoin;
+    Monster *srcMon, *dstMon;
+
+
+
+
+    modelDst->chicken.x = modelSrc->chicken.x;
+    modelDst->chicken.y = modelSrc->chicken.y;
+
+
+    modelDst->chicken.velocity = modelSrc->chicken.velocity;
+    modelDst->chicken.isFalling = modelSrc->chicken.isFalling;
+    modelDst->chicken.max_y = modelSrc->chicken.max_y;
+    modelDst->chicken.dead = modelSrc->chicken.dead;
+
+
+    modelDst->score.total = modelSrc->score.total;
+    modelDst->score.prev_total = modelSrc->score.prev_total;
+    modelDst->score.digits = modelSrc->score.digits;
+   
+    srcMon = modelSrc->monster;
+    dstMon = modelDst->monster;
+    for(j = 0; j < MAX_MONSTER; j++)
+    {
+        dstMon->x = srcMon->x;
+        dstMon->y = srcMon->y;
+
+
+        srcMon++;
+        dstMon++;    
+    }
+
+
+    srcCoin = (modelSrc->coins);
+    dstCoin = (modelDst->coins);
+
+    for(i=0; i < MAX_COINS;i++)
+    {
+        dstCoin->x = srcCoin->x;
+        dstCoin->y = srcCoin->y;
+        srcCoin++;
+        dstCoin++;    
+    }
+}
+
+
+
+
+/***********************************************************************
+* Name: process_synchronous_events
+*
+* Purpose: Processes synchronous events for the game.
+*
+* Details: Calls functions to handle animal movement, screen shifting,
+*          off-screen replacement, and animal death checking.
+*
+* Parameters:
+*     - model: Pointer to the game model.
+*     - endGame: Pointer to a boolean indicating game end.
+*     - seed: Random seed for event generation.
+***********************************************************************/
+void process_synchronous_events(Model *model, bool *endGame, int seed)
+{
+    animal_horizontal_movement((model));
+
+
+    check_animal_death(model, endGame);
+}
+
+/***********************************************************************
+* Name: get_time
+*
+* Purpose: Retrieves the current system time in milliseconds.
+*
+* Details: Gets the system time value stored at memory address 0x462,
+*
+* Returns:
+*     - UINT32: The current system time in milliseconds.
+***********************************************************************/
+UINT32 get_time() {
+    UINT32 time;
+    long old_ssp = Super(0);
+    time = *((UINT32 *)0x462);
+    Super(old_ssp);
+    return time;
+}
+
+
