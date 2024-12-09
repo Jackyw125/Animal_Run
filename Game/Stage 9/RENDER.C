7b3d2929d86dfb25796c0bf01b8ab63c46ed9221
Jackyw125 <133839896+Jackyw125@users.noreply.github.com>
2024-12-09 08:42:21 -0700

    Update RENDER.C

--- Game/Stage 9/RENDER.C
+++ Game/Stage 9/RENDER.C
@@ -30,8 +30,8 @@ void render(Model *model, UINT32 *base) {
         clear_bitmap_32(base, model->chicken.prev_x, model->chicken.prev_y, clear_bitmap, CHICKEN_HEIGHT);
         model->chicken.prev_x = model->chicken.x;
         model->chicken.prev_y = model->chicken.y;
-        render_animal(&(model->chicken), base);
     }
+    render_animal(&(model->chicken), base);
 
     for (i = 0; i < MAX_COINS; i++){
         if (check_collision_coin(&model->chicken, model->coins, i))
